ebdbc73f33902d2f207311b6f006d7e2e7d77eb9
snorre <snorre@10.102.49.94>
2012-09-17 19:26:05 +0200

    Added an empty test class for event list activity class.

---
+++ GeoMusicTest/src/no/uib/info331/geomusic/test/EventListActivityTest.java
@@ -0,0 +1,15 @@
+package no.uib.info331.geomusic.test;
+
+import no.uib.info331.geomusic.EventListActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class EventListActivityTest extends
+		ActivityInstrumentationTestCase2<EventListActivity> {
+	
+	public EventListActivityTest() {
+		super(EventListActivity.class);
+	}
+	
+	
+
+}
