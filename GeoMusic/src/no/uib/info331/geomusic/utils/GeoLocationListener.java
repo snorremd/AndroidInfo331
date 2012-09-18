01dcdc199deaf3e0f332fae0086c37a2db6a3b59
snorre <snorre@10.102.49.94>
2012-09-18 09:47:56 +0200

    Added a GeoLocationListener class that we can use for getting GPS locations. The class implements LocationListener with all relevant methods. Some methods currently only print debug messages. The class constructor takes as parameter a LocationManager so the location search operation can be switched off when a location is found.

---
+++ GeoMusic/src/no/uib/info331/geomusic/utils/GeoLocationListener.java
@@ -0,0 +1,66 @@
+package no.uib.info331.geomusic.utils;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+import android.util.Log;
+
+public class GeoLocationListener implements LocationListener {
+	
+	private LocationManager locManager;
+	
+	public GeoLocationListener(LocationManager locManager) {
+		super();
+		this.locManager = locManager;
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// Make sure we have a connection
+		if(location != null) {
+			// Stop listening for gps location to save battery
+			locManager.removeUpdates(this);
+			
+			double latitude = location.getLatitude();
+			double longtitude = location.getLongitude();
+		}
+
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Make a callback to an activity to start gps setting intent
+		Log.d("GeoLocationListener", "GPS provider disabled");
+
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		Log.d("GeoLocationListener", "GPS provider enabled.");
+
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		Log.d("GeoLocationListener", "GPS status changed.");
+		
+		// Do a switch case and debug the new state of the GPS
+		switch (status) {
+		case LocationProvider.AVAILABLE:
+			Log.d("GeoLocationListener", "GPS LocationProvider changed to available");
+			break;
+		case LocationProvider.TEMPORARILY_UNAVAILABLE:
+			Log.d("GeoLocationListener", "GPS LocationProvider changed to temporarily unavilable");
+			break;
+		case LocationProvider.OUT_OF_SERVICE:
+			Log.d("GeoLocationListener", "GPS LocationProvider changed to out of service");
+
+		}
+
+	}
+
+}
