2a5193b84a36c83ef9b9966cb46ac66b8e34cba3
Snorre Magnus Davoen <snorremd@gmail.com>
2012-09-18 11:30:19 +0200

    Added an empty implementation of FetchEventListAsyncTask. OnPreExecute-method should fetch necessary resources (objects and connections) needed for the event fetching task. The doInBackground should do the Last.fm-API-call. onPostExecute-method should release the GPS. onProgressUpdate should perhaps return the amount of event objects created.

---
+++ GeoMusic/src/no/uib/info331/geomusic/utils/FetchEventListAsyncTask.java
@@ -0,0 +1,37 @@
+package no.uib.info331.geomusic.utils;
+
+import java.util.List;
+
+import de.umass.lastfm.Event;
+
+import android.location.Location;
+import android.os.AsyncTask;
+
+public class FetchEventListAsyncTask extends
+		AsyncTask<Location, Integer, List<Event>> {
+
+	@Override
+	protected void onPreExecute() {
+		// TODO Auto-generated method stub
+		super.onPreExecute();
+	}
+	
+	@Override
+	protected List<Event> doInBackground(Location... locations) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	protected void onPostExecute(List<Event> result) {
+		// TODO Auto-generated method stub
+		super.onPostExecute(result);
+	}
+	
+	@Override
+	protected void onProgressUpdate(Integer... values) {
+		// TODO Auto-generated method stub
+		super.onProgressUpdate(values);
+	}
+
+}
