5972566765af2f293d06e00a9c7e2612fe3175a4
Per Andersen <post@perandersen.no>
2012-09-19 11:33:22 +0200

    Added a new test class and a new layout for activity event info

--- GeoMusic/src/no/uib/info331/geomusic/EventInfoActivity.java
+++ GeoMusic/src/no/uib/info331/geomusic/EventInfoActivity.java
@@ -1,7 +1,21 @@
 package no.uib.info331.geomusic;
 
+import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
 
 public class EventInfoActivity extends Activity {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_info);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_event_info, menu);
+        return true;
+    }
 }
