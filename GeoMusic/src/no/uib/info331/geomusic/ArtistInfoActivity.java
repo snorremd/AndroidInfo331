69e80f3eb581d2381df4089fb4ebfb8b94b705ea
uma612 <dolly.6.12@gmail.com>
2012-10-04 13:06:28 +0200

    5. As a user I want to open an artist's most popular song in the Spotify app. (2)
    Status: In progress
    
    Files: FetchPopularTrackTask
    Added an async task class to popular tracks of an artist by artistname.

--- GeoMusic/src/no/uib/info331/geomusic/ArtistInfoActivity.java
+++ GeoMusic/src/no/uib/info331/geomusic/ArtistInfoActivity.java
@@ -4,19 +4,32 @@ import java.io.InputStream;
 import java.net.URL;
 
 import no.uib.info331.geomusic.utils.FetchArtistAsyncTask;
+import no.uib.info331.geomusic.utils.FetchEventListAsyncTask;
+import no.uib.info331.geomusic.utils.FetchPopularTrackTask;
 
 import de.umass.lastfm.Artist;
 import de.umass.lastfm.ImageSize;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ArtistInfoActivity extends Activity {
+	
+	String artist;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,8 +37,21 @@ public class ArtistInfoActivity extends Activity {
         setContentView(R.layout.activity_artist_info);
         
         Intent intent = getIntent();
-        String artist = intent.getStringExtra("ArtistName");
+        artist = intent.getStringExtra("ArtistName");
         new FetchArtistAsyncTask(this).execute(artist);
+        
+        Button trackButton=(Button)findViewById(R.id.popularTracks);
+        trackButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				FetchPopularTrackTask fetchPopularTrack = new FetchPopularTrackTask(ArtistInfoActivity.this);
+				fetchPopularTrack.execute(artist);
+				
+			}
+		});
+        
     }
 
     @Override
@@ -51,5 +77,29 @@ public class ArtistInfoActivity extends Activity {
 		
 		TextView artistSummary = (TextView)findViewById(R.id.artist_summary_text);
 		artistSummary.setText(result.getWikiSummary());
+	}
+	public void playPopularTrack(String trackname){
+		
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+		//Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+		//startActivity(intent);
+	intent.setAction(MediaStore.INTENT_ACTION_MEDIA_PLAY_FROM_SEARCH);
+		intent.setComponent(new ComponentName("com.spotify.mobile.android.ui", "com.spotify.mobile.android.ui.Launcher"));
+		intent.putExtra(SearchManager.QUERY, trackname);
+		
+		try {
+		  startActivity(intent);
+			//startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id="+appName)));
+		}catch (ActivityNotFoundException e) {
+		  Toast.makeText(this, "You must first install Spotify", Toast.LENGTH_LONG).show();  
+		 // Intent i = new Intent(Intent.ACTION_VIEW,Uri.parse("market://details?id=com.spotify.mobile.android.ui"));
+		 Intent i= new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=com.spotify.mobile.android.ui"));
+		  
+		 // startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id="+appName)));
+		  startActivity(i);
+		}
+		
+		
+	
 	}
 }
